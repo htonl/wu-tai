68a8caf46abef8bf6451821cf765de8ab56aa92c
Luke Cesarz <lcesarz@pm.me>
2021-08-04 23:46:01 -0700

    Add code for initial test case
    This grabs all traffic on wire, interprets the traffic
    as IPv4, grabs the dest addr, gets the hostname, and
    prints it.
    
    Next is to figure out how to do inspection. Maybe we want to
    try to rip the name out of the packet? Maybe we use a static
    list of IP's from the targeted ad services, etc. Fun problems
    to solve now...

--- main.c
+++ main.c
@@ -1,57 +1,112 @@
-#include<netinet/in.h>
-#include<errno.h>
-#include<netdb.h>
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<netinet/ip_icmp.h>
-#include<netinet/udp.h>
-#include<netinet/tcp.h>
-#include<netinet/ip.h>
-#include<netinet/if_ether.h>
-#include<net/ethernet.h>
-#include<sys/socket.h>
-#include<arpa/inet.h>
-#include<sys/ioctl.h>
-#include<sys/time.h>
-#include<sys/types.h>
-#include<unistd.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netdb.h>
+#include <netinet/ip_icmp.h>
+#include <netinet/udp.h>
+#include <netinet/tcp.h>
+#include <netinet/ip.h>
+#include <netinet/if_ether.h>
+#include <net/ethernet.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <sys/ioctl.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 // Globals
 #define RECVBUFSIZE 65535
-FILE *log;
+#define wt_log printf
+FILE *logfile;
 
-#define wt_log(
+/**
+ * Method for getting hostname of given addr
+ * @param addr ipv4 address to lookup
+ * @return pointer to allocated buffer containing hostname
+ *
+ */
+char *get_hostname(const struct sockaddr_in *addr)
+{
+    char *hostname;
+    int res;
+    
+    hostname = (char*)malloc(NI_MAXHOST);
+    memset(hostname, 0, NI_MAXHOST);
+    if (hostname == NULL)
+    {
+        wt_log("Unable to allocate buffer for hostname\n");
+        goto err;
+    }
+    res = getnameinfo((struct sockaddr *)addr, sizeof(struct sockaddr),
+                      hostname, NI_MAXHOST,
+                      NULL, 0, 0);
+    if (res)
+    {
+        printf("error: %s\n", gai_strerror(res));
+        goto err;
+    }
+    return hostname;
+err:
+    if (hostname) {
+        free(hostname);
+        hostname = NULL;
+    }
+    return NULL;
+}
 
 void inspect_packet(unsigned char *buf, unsigned long bufsize)
 {
     struct iphdr *iph;
-    struct in_addr source, dest; 
+    struct sockaddr_in source, dest;
+    char *dest_hostname;
     
     // Grab the ip header 
     iph = (struct iphdr*)(buf + sizeof(struct ethhdr));
-    source.s_addr = iph->saddr;
-    dest.s_addr = iph->daddr;
+    // Grab the src/dest addrs
+    source.sin_addr.s_addr = iph->saddr;
+    dest.sin_addr.s_addr = iph->daddr;
+    // TODO v4 or v6?
+    /* TODO: Some thoughts on design:
+     *
+     * 1 - We get a list of IP's for the hostname, and then use that for
+     * matching. This would be faster, but poses the risk of missing a new
+     * IP that is added dynamically. We can update the list occasionally to
+     * update it. This seems much more reasonable than 2. 
+     *
+     * 2 - We do a lookup everytime (after checking a cache of known
+     * IP->Hostnames, this seems to have unreasonable overhead.
+     */
+    dest.sin_family = AF_INET;
+    dest_hostname = get_hostname(&dest);
+    if (dest_hostname != NULL && strcmp(dest_hostname, "localhost"))
+    {
+        printf("dest_hostname: %s\n", dest_hostname);
+        free(dest_hostname);
+    }
+}
 
     
-
 int main(void)
 {
-    unsigned long saddr_size, bufsize;
+    unsigned long saddrlen, bufsize;
     struct sockaddr saddr;
-    unsigned char buf;
+    unsigned char *buf;
     struct sockaddr_in source, dest;
     int raw_sock;
 
-    buf = malloc(RECVBUFSIZE);
+    buf = (unsigned char*) malloc(RECVBUFSIZE);
     if (!buf) {
         printf("Failed to alloc receive buffer\n");
         buf = NULL;
         goto error;
     }
     // initialize log file
-    log = fopen("log.txt", "w");
-    if (!log)
+    logfile = fopen("log.txt", "w");
+    if (!logfile)
     {
         printf("Unable to create logfile");
         goto error;
@@ -59,7 +114,7 @@ int main(void)
     // open our raw socket to collect the packets
     // TODO I wonder if ETH_P_ALL is overkill?
     raw_sock = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
-    if (sock_raw < 0)
+    if (raw_sock < 0)
     {	
         perror("Failed to open Socket");
         goto error;
@@ -70,8 +125,8 @@ int main(void)
     {
         saddrlen = sizeof(struct sockaddr);
         //Receive a packet
-        bufsize = recvfrom(sock_raw, buffer, 65536, 0, &saddr, (socklen_t*)&saddrlen);
-        if(data_size < 0)
+        bufsize = recvfrom(raw_sock, buf, 65536, 0, &saddr, (socklen_t*)&saddrlen);
+        if(bufsize < 0)
         {
             printf("Failed to receive packets\n");
             goto error;
